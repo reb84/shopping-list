c390611751522a9819c56c86aec24ef6833eb730
Reb84 <152401972+reb84@users.noreply.github.com>
2025-10-20 19:23:49 +0100

    Add clear all button

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
-// const emojis = ['🐥','🐯','🐼']
-// console.log(emojis.includes('🐴'))
+const addItemBtn = document.getElementById("add-item-btn");
+const clearAllItems = document.getElementById("clear-btn");
+const itemInput = document.getElementById("item-input");
+const list = document.getElementById("list");
 
-const addItemBtn = document.getElementById('add-item-btn')
-const itemInput = document.getElementById('item-input')
-const list = document.getElementById('list')
+const shoppingList = [];
 
-const shoppingList = []
-    
-addItemBtn.addEventListener('click', function(){
-/*
-Challenge:
-1. Add an if else to the event listener's function.
-2. Only add an item to the shoppingList array if it 
-   is not already in the shoppingList array.
-3. If an item is a duplicate, clear the input field
-   and log out "no duplicates".
-*/
-        if(shoppingList.includes(itemInput.value)){
-            console.log('no duplicates')
-        }
-        else{
-            shoppingList.push(itemInput.value)
-            render()        
-        }
-        itemInput.value = '' 
+addItemBtn.addEventListener("click", function () {
+  if (shoppingList.includes(itemInput.value)) {
+    console.log("no duplicates");
+  } else {
+    shoppingList.push(itemInput.value);
+    render();
+  }
+  itemInput.value = "";
+});
+
+
+clearAllItems.addEventListener("click", function () {
+  shoppingList.length = 0;
+  render();
 })
 
-function render(){    
-    let html = ''
-    for (let item of shoppingList){
-        html+= `<li class="list-item">${item}</li>`
-    }
-    list.innerHTML = html
+function render() {
+  let html = "";
+  for (let item of shoppingList) {
+    html += `<li class="list-item">${item}</li>`;
+  }
+  list.innerHTML = html;
 }
 
-render()
-
+render();
